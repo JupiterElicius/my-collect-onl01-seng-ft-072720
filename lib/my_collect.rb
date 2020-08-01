e09db1be23cfeab40b6ee65c940518c6f0be80ed
Nick Bazaldua <bazalnick@gmail.com>
2020-08-01 00:37:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  i = 0
+  new_array = [] #store the modified collection
+  while i < array.length #iterating every value in collection
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
 
+
+
+
+
+
+
+
+#define method that takes in an argument of a collection-
+#iterate over collection using a while loop
+#execute code the code in the block that is called with for each element (yield)
+#return modified collection (going to need a new empty array to hold this)
 
