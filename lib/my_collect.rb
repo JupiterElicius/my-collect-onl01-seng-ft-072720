6cd27e4119759c5761e7cf03fee772497f61fb86
Nick Bazaldua <bazalnick@gmail.com>
2020-08-01 00:08:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,4 +10,6 @@ def my_collect(array)
 
 #define method that takes in an argument of a collection 
 #iterate over collection using a while loop
-#execute code in b
+#execute code the code in the block that is called with for each element (yield)
+#return modified collection (going to need a new empty array to hold this)
+
